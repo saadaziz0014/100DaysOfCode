e5f2f2af6dd508d2ad67e1f205f222a20abc2987
Saad <saadaziz0014gmail.com>
2022-12-14 19:35:11 +0500

    day 17

---
+++ 17.py
@@ -0,0 +1,16 @@
+abc = "Welcome to Saad Channel"
+
+for ch in abc:
+    print(ch, end=",")
+
+print()
+
+for i in range(5):
+    print(i, end=" ")
+
+print()
+
+for k in range(1,12,2):
+    print(k,end=" ")
+
+print()
