aaea048790d07a98a440c98f7097ae2604fca0f0
Saad <saadaziz0014gmail.com>
2022-12-18 18:36:18 +0500

    Day 21

---
+++ 21.py
@@ -0,0 +1,40 @@
+def nameF(fn="muhammad",mn="saad",ln="aziz"):
+    return fn + mn + ln
+
+
+n = nameF()
+print(n)
+
+n = nameF("ahmad")
+print(n)
+
+
+n = nameF(mn="hadi")
+print(n)
+
+def avg(a, b = 4):
+    return (a+b)/2
+
+c = avg(4)
+print(c)
+
+def sum(*num):
+    sum = 0
+    for i in num:
+        sum = sum + i
+    
+    print("Sum = ", sum)
+
+
+sum(4)
+sum(4,4,8)
+sum(4,2,3,6,4,5)
+sum(4,1,1)
+sum(4,4,7,8)
+
+
+def dictP(**dictdata):
+    print("Name = ",dictdata["name"]," and Age = ",dictdata["age"])
+
+
+dictP(name="Saad",age=22)
