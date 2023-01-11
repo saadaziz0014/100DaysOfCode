b666da9930c0034653961e0dd6ccd619eec1b49d
Saad <saadaziz0014gmail.com>
2023-01-11 23:27:36 +0500

    Day 44

---
+++ 43.py
@@ -0,0 +1,1 @@
+# Virtual Environment in python
---
+++ 44.py
@@ -0,0 +1,11 @@
+# import module
+
+# import math
+# import math as ma
+# from math import floor
+from math import sqrt, pi as P
+
+# print(math.sqrt(36))
+# print(ma.sqrt(25))
+# print(floor(5.32))
+print(sqrt(64) * P)
