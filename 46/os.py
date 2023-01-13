d1e2e1df862b560aa7c2bf274a1c95511db383c5
Saad <saadaziz0014gmail.com>
2023-01-13 18:17:51 +0500

    Day 46

---
+++ 46/os.py
@@ -0,0 +1,12 @@
+import os
+
+# os.mkdir("createWithOSModule")
+
+print(os.getcwd())
+
+# newD = "D:/Ultimate Python/46/createWithOSModule"
+# os.chdir(newD)
+
+with open("newFile.txt", "w") as fp:
+    pass
+
